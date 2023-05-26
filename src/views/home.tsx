20c8d688be8d866845ffdb80e4500d80a4524efd
korealu <643949593@qq.com>
2023-05-26 15:50:32 +0800

    feat(husky): husky-集成配置
    husky-集成配置

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode, memo } from 'react'
+import React, { memo } from 'react'
+import type { FC, ReactNode } from 'react'
 
 interface IProps {
   children?: ReactNode
 }
-const Home: React.FC<IProps> = () => {
-  return <div>demo</div>
+
+const Home: FC<IProps> = () => {
+  return <div>Home</div>
 }
 
 export default memo(Home)
